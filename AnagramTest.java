8b7b8572fac10129acd40300cafb39e841530711
Jeancy Tshibemba Mukendi <jeancyts@gmail.com>
2020-04-01 20:59:20 +0200

    First Version Of AnagramTest Program
    This is the first version of anagram test program using loops structure and if statement plus the StringBuilder class.

---
+++ AnagramTest.java
@@ -0,0 +1,30 @@
+public class AnagramTest {
+	
+	private static boolean isAnagram(String str1 , String str2){
+		StringBuilder leftString = new StringBuilder( str1.trim().toLowerCase());
+		StringBuilder rightString = new StringBuilder( str2.trim().toLowerCase());
+        if(leftString.length() != rightString.length()){
+			return false;
+		}
+		int l = leftString.length();
+		int k = rightString.length();
+		for(int i = 0; i < l < i++){
+			for(int j = o; j < k ; j{
+				if(leftString.charAt(i) == rightString.charAt(j)){
+					leftString.deleteCharAt(i);
+					rightString.deleteCharAt(j);
+					//Leaving the inner loop to avoid deleting twice
+					break; 
+				}
+			}
+		}
+		//Testing if both strings have zero character
+		if(leftString.length() == 0) && (rightString.length()==0){
+			return true;
+		}else {
+			return false;
+		}
+			
+		
+	}
+}
