1da281ab64a27d7a51aa1c6de4678dc07926b220
Jeancy Tshibemba Mukendi <jeancyts@gmail.com>
2020-04-04 16:36:12 +0200

    Adding more test cases

--- AnagramTest/src/main/java/com/mycompany/anagramtest/AnagramTest.java
+++ AnagramTest/src/main/java/com/mycompany/anagramtest/AnagramTest.java
@@ -5,34 +5,22 @@ package com.mycompany.anagramtest;
 import java.util.Arrays;
 
 public class AnagramTest {
-	/**
+    /** 
     *@param str1 str2
     *@return return true if both strings are anagram of each other and false otherwise
     *@author Jeancy Tshibemba
     *@since April 2020
     */
 	private static boolean isAnagram(String str1 , String str2){
-		// Converting both strings to char arrays as strings do not have direct
-		// sorting method in java.
-		char [] leftArray = ( str1.trim().toLowerCase()).toCharArray();
-		char [] rightArray = ( str2.trim().toLowerCase()).toCharArray();
-		Arrays.parallelSort(leftArray);
-		Arrays.parallelSort(rightArray);
-		
-		if(leftArray.length != rightArray.length){
-		        return false;
-		}
-		// Both char arrays have the same number of characters
-		for(int i = 0; i < leftArray.length; i++){
-            if(leftArray[i] != rightArray[i]){
-                return false;  
-            }
-		}
-		// We only get to this stage if all the elements pass the if test
-		return true;
-	}
-				    
-	public static void main(String [] args)	{
+            // Converting both strings to char arrays as strings do not have direct
+            // sorting method in java.
+            char [] leftArray = ( str1.trim().toLowerCase()).toCharArray();
+            char [] rightArray = ( str2.trim().toLowerCase()).toCharArray();
+            Arrays.parallelSort(leftArray); // Sorting the first array
+            Arrays.parallelSort(rightArray); // Sorting the second array
+            return Arrays.equals(leftArray, leftArray);
+        }			    
+	public static void main(String [] args){
             //Test cases;
             String a = "integral"; // initializing first string
             String b = "Triangle"; // initializing second string
